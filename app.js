c4169ee989065b9f4c59b3361701721cfb3c4ac4
Eric King <e.king117@gmail.com>
2016-01-07 20:01:38 -0500

    Added nodemon

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express();
 
-var port = 8080;
+var port = process.env.PORT || 8080;
 
 app.use(express.static('public'));
 app.use(express.static('src/views'));
@@ -18,3 +18,21 @@ app.get('/books', function (req, res){
 app.listen(port, function(err){
   console.log('running server on port ' + port);
 });
+
+
+
+function getPonyAllergies (ponies, ownerEmail) {
+  // Write your code here, and
+  // return your final answer.
+  var userPonies = ponies.filter(function(pony){
+    return pony.email === ownerEmail;
+  });
+  var allUserAllergies = Array.prototype.concatAll(
+    userPonies.map(function(pony){
+      return pony.allergies;
+    })
+  );
+  return arr.allUserAllergies(function(a, b){
+    return a[0] > b[0];
+  });
+}
