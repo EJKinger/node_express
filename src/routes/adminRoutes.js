ba27166b6b950bc14a05d9a64a3c1adc8d52e808
Eric King <e.king117@gmail.com>
2016-01-22 20:27:29 -0500

    Added mongodb

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,10 +1,38 @@
 var express = require('express');
 var adminRouter = express.Router();
+var mongodb = require('mongodb').MongoClient;
+
+var books = [
+  {
+    title: 'War and Peace',
+    author: 'Lev Nikolayevich Tolstoy'
+  },
+  {
+    title: 'Les Miserables',
+    author: 'Victor Hugo'
+  },
+  {
+    title: 'Frankenstein',
+    author: 'Mary Shelly'
+  },
+  {
+    title: 'The Power Broker',
+    author: 'Robert Caro'
+  }
+];
 
 var router = function(nav){
   adminRouter.route('/addBooks')
     .get(function(req, res){
-      res.send('inserting books');
+
+      var url = 'mongodb://localhost:27017/libraryApp';
+      mongodb.connect(url, function(err, db){
+        var collection = db.collection('books');
+        collection.insertMany(books, function(err, results){
+          res.send(results);
+          db.close();
+        });
+      });
     });
   return adminRouter;
 };
--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,44 +1,46 @@
 var express = require('express');
 var bookRouter = express.Router();
-
-var books = [
-  {
-    title: 'War and Peace',
-    author: 'Lev Nikolayevich Tolstoy'
-  },
-  {
-    title: 'Les Miserables',
-    author: 'Victor Hugo'
-  },
-  {
-    title: 'Frankenstein',
-    author: 'Mary Shelly'
-  },
-  {
-    title: 'The Power Broker',
-    author: 'Robert Caro'
-  }
-];
+var mongodb = require('mongodb').MongoClient;
+var objectID = require('mongodb').ObjectID;
 
 var router = function(nav){
   bookRouter.route('/')
     .get(function (req, res){
-      res.render('bookListView', { 
-        nav: nav,
-        title: 'Books',
-        books: books
+      var url = 'mongodb://localhost:27017/libraryApp';
+      mongodb.connect(url, function(err, db){
+        var collection = db.collection('books');
+        collection.find({}).toArray(function(err, results){
+          res.render('bookListView', { 
+            nav: nav,
+            title: 'Books',
+            books: results
+          });
+        });
       });
     }
   );
 
   bookRouter.route('/:id')
     .get(function (req, res){
-      var id = req.params.id;
-      res.render('bookView', { 
-        nav: nav,
-        title: 'Books',
-        book: books[id]
+      var id = new objectID(req.params.id);
+      var url = 'mongodb://localhost:27017/libraryApp';
+      mongodb.connect(url, function(err, db){
+        var collection = db.collection('books');
+        collection.findOne({_id: id}, function(err, results){
+          res.render('bookView', { 
+            nav: nav,
+            title: 'Books',
+            book: results
+          });
+        });
       });
+
+      // var id = req.params.id;
+      // res.render('bookView', { 
+      //   nav: nav,
+      //   title: 'Books',
+      //   book: books[id]
+      // });
     }
   );
 
