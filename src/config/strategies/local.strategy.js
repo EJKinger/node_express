0e666bf9e76f89780e68913411c7e0af618fd64c
Eric King <e.king117@gmail.com>
2016-01-23 20:26:34 -0500

    Added auth

---
+++ src/config/strategies/local.strategy.js
@@ -0,0 +1,19 @@
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var mongodb = require('mongodb').MongoClient;
+
+module.exports = function(){
+  passport.use(new LocalStrategy({
+    usernameField: 'userName',
+    passwordField: 'password'
+  }, function (username, password, done){
+      var url = 'mongodb://localhost:27017/libraryApp';
+      mongodb.connect(url, function (err, db){
+        var collection = db.collection('users');
+        collection.findOne({userName: username}, function (err, results){
+          var user = results;
+          done(null, user);
+        });
+    });
+  }));
+};
